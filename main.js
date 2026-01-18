8c0ad8fb60896b3061006bdf4b5591078fae6ef1
Mariano Aloi <mariano@aloi.com.br>
2026-01-18 21:14:57 +0100

    add spell check

--- main.js
+++ main.js
@@ -1,7 +1,74 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+const { Menu, MenuItem } = require('electron');
+
 let mainWindow;
+
+const contextMenu = (mainWindow) => {
+  // Sets the spellchecker to check English US and French
+mainWindow.webContents.session.setSpellCheckerLanguages(['en-US', 'it-IT', 'pt-BR']);
+
+// An array of all available language codes
+const possibleLanguages = mainWindow.webContents.session.availableSpellCheckerLanguages
+console.log('Available languages:', possibleLanguages);
+
+// Listen for the context-menu event to show a custom context menu
+  mainWindow.webContents.on('context-menu', (e, params) => {
+    const menu = [];
+
+    if (params.selectionText) {
+      menu.push({
+        label: 'Copy',
+        role: 'copy'
+      });
+
+      menu.push({
+        label: 'Cut',
+        role: 'cut'
+      });
+    } else {
+      menu.push({
+        label: 'Paste',
+        role: 'paste'
+      });
+    }
+
+
+    menu.push({
+      label: 'Undo',
+      role: 'undo'
+    });
+
+    menu.push({
+      label: 'Redo',
+      role: 'redo'
+    });
+
+    const contextMenu = Menu.buildFromTemplate(menu);
+
+    contextMenu.append(new MenuItem({ type: 'separator' }));
+
+    for (const suggestion of params.dictionarySuggestions) {
+      contextMenu.append(new MenuItem({
+        label: suggestion,
+        click: () => mainWindow.webContents.replaceMisspelling(suggestion)
+      }))
+    }
+
+    contextMenu.append(new MenuItem({ type: 'separator' }));
+    // Allow users to add the misspelled word to the dictionary
+    if (params.misspelledWord) {
+      contextMenu.append(
+        new MenuItem({
+          label: 'Add to dictionary',
+          click: () => mainWindow.webContents.session.addWordToSpellCheckerDictionary(params.misspelledWord)
+        })
+      )
+    }
+    contextMenu.popup();
+  });
+};
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -15,9 +82,11 @@ function createWindow() {
     }
   });
 
+  contextMenu(mainWindow)
+
   // Load the Angular app
   const isDev = process.env.NODE_ENV === 'development';
-  
+
   if (isDev) {
     mainWindow.loadURL('http://localhost:4200');
     mainWindow.webContents.openDevTools();
